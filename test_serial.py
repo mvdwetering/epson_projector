38bee24fc09c47d6b80b992e1c6bba038e404fa0
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2025-01-13 19:40:08 +0100

    Make host commandline argument on test_serial.py

--- test_serial.py
+++ test_serial.py
@@ -1,27 +1,31 @@
+#!/usr/bin/env python3
+
 import asyncio
+import argparse
 import epson_projector as epson
-from epson_projector.const import (POWER, PWR_ON, PWR_OFF)
+from epson_projector.const import POWER, PWR_ON, PWR_OFF
 import logging
 
 _LOGGER = logging.getLogger(__name__)
 
 console_handler = logging.StreamHandler()
-console_handler.setFormatter(logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+console_handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+)
 _LOGGER.addHandler(console_handler)
 _LOGGER.setLevel(logging.DEBUG)
 
 logging.basicConfig(level=logging.DEBUG)
 
-async def main_serial():
+
+async def main_serial(args):
     """Run main with serial connection."""
-    await run()
+    await run(args)
 
 
-async def run():
-    projector = epson.Projector(host='/dev/ttyUSB0',
-                                type='serial',
-                                timeout_scale=2.0)
+async def run(args):
+    projector = epson.Projector(host=args.host, type="serial", timeout_scale=2.0)
+
     data = await projector.get_power()
     print(data)
     cmd = None
@@ -35,8 +39,19 @@ async def run():
 
     serialno = await projector.get_serial_number()
     print("Projector serial number:", serialno)
+
     projector.close()
 
+
+parser = argparse.ArgumentParser(
+    description="Test application for serial EPSON projectors."
+)
+parser.add_argument(
+    "host",
+    help="Serial port name like '/dev/ttyUSB0' or PySerial URL handler like 'socket://192.168.178.42:12345'",
+)
+args = parser.parse_args()
+
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main_serial())
+loop.run_until_complete(main_serial(args))
 loop.close()
