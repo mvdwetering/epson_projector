75e299e6a27c782266026608404a77f9feadf6f1
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2026-07-08 21:11:53 +0200

    Export types from package

--- epson_projector/__init__.py
+++ epson_projector/__init__.py
@@ -3,5 +3,13 @@
 from epson_projector.error import ProjectorError, ProjectorUnavailableError
 
 from epson_projector.projector import Projector
+from epson_projector.enums import KeyCodes
 
 from epson_projector.version import __version__
+
+__all__ = [
+    "Projector",
+    "KeyCodes",
+    "ProjectorError",
+    "ProjectorUnavailableError",
+]
